117d06baa96a6d5ba54637cf41282c272e8bdead
charles-r-earp <charles.r.earp@gmail.com>
2017-03-18 06:01:46 -0700

    Update lambda.hpp

--- include/lambda.hpp
+++ include/lambda.hpp
@@ -1,3 +1,6 @@
+#ifndef LAMBDA_LAMBDA_HPP
+#define LAMBDA_LAMBDA_HPP
+
 #include "lambda_internal.hpp"
 
 namespace lambda {
@@ -6,4 +9,6 @@ namespace lambda {
     template<typename T> using Lambda = internal::Lambda<T>;
 
 }
+
+#endif // LAMBDA_LAMBDA_HPP
 
